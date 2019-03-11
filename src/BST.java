4cc5b11b2d1f4bb3b3d210a51027d1bd1741c773
Takudzwa Barnabas Shumbamhini <shmtak004@sl-dual-206.cs.uct.ac.za>
2019-03-11 18:38:35 +0200

    Monday Edits 2

--- src/BST.java
+++ src/BST.java
@@ -8,16 +8,13 @@ public class BST
     private static int count_two=0;
     public void put( String key, Object value1, Object value2)
     {
-	count_two++;
         if ( root == null )
 		
         {
-	    //count_two++;
             root = new BSTNode( key, value1, value2);
         }
         else
         {
-	    //count_two++;
             root.put( key, value1, value2);
         }
     }
@@ -25,7 +22,6 @@ public class BST
     public Object get( String key)
 	
     {
-	    count_two++;
         return root == null ? null : root.get( key );
     }
    public int getCount_two(){
