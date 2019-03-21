4ea90da0def9f0757db5ff80f9d740ca37bb0c12
Takudzwa Barnabas Shumbamhini <shmtak004@sl-dual-207.cs.uct.ac.za>
2019-03-21 13:34:07 +0200

    Human Rights Day Changes

--- bin/AVLGraph.py
+++ bin/AVLGraph.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import matplotlib.pyplot as plt
 
-#BEST Graph
+#BEST Graph AVL
 Counts_BEST=[]
 bst=[]
 a=0
@@ -12,7 +12,7 @@ for line in file.readlines():
 	bst.append(a)
 	a+=1
 
-#Average Graph
+#Average Graph AVL
 Counts_AVG=[]
 avg=[]
 b=0
@@ -23,7 +23,7 @@ for line in file.readlines():
 	avg.append(b)
 	b+=1
 
-#Worst Graph
+#Worst Graph AVL
 Counts_WORST=[]
 worst=[]
 c=0
@@ -34,15 +34,55 @@ for line in file.readlines():
 	worst.append(c)
 	c+=1
 
-plt.title('Graph of Number of Count Operations Against Number of Sampled Data Items for AVLTree')
+#BEST Graph BST
+CountsBST_BEST=[]
+bst_BST=[]
+a_BST=0
+file=open("PowerBSTOutputs/min.txt", "r")
+for line in file.readlines():
+	float_count=int(line)
+	CountsBST_BEST.append(float_count)
+	bst_BST.append(a_BST)
+	a_BST+=1
 
+#Average Graph BST
+CountsBST_AVG=[]
+avg_BST=[]
+b_BST=0
+file=open("PowerBSTOutputs/avg.txt", "r")
+for line in file.readlines():
+	float_count=int(line)
+	CountsBST_AVG.append(float_count)
+	avg_BST.append(b_BST)
+	b_BST+=1
+
+#Worst Graph BST
+CountsBST_WORST=[]
+worst_BST=[]
+c_BST=0
+file=open("PowerBSTOutputs/max.txt", "r")
+for line in file.readlines():
+	float_count=int(line)
+	CountsBST_WORST.append(float_count)
+	worst_BST.append(c_BST)
+	c_BST+=1
+
+
+plt.title('Graph of Number of Count Operations Against Number of Sampled Data Items for AVLTree')
 plt.subplot(3,1,1)
-plt.ylabel('No. of Count Operations')
-plt.xlabel('No. of Sampled Data Items')
 plt.plot(bst,Counts_BEST, 'g--'  )
-#plt.plot(avg,Counts_AVG, 'b--'  )
+plt.plot(bst_BST,CountsBST_BEST, 'k--'  )
+plt.ylabel('Best Case')
+
 plt.subplot(3,1,2)
+plt.ylabel('No. of Count Operations')
 plt.plot(avg,Counts_AVG, 'b--'  )
+plt.plot(avg_BST,CountsBST_AVG, 'k--'  )
+
 plt.subplot(3,1,3)
 plt.plot(worst,Counts_WORST, 'r--'  )
+plt.plot(worst_BST,CountsBST_WORST, 'k--'  )
+plt.xlabel('No. of Sampled Data Items')
+plt.ylabel('Worst Case')
+
 plt.show()
